4cfec787a677dc43d6fb412913e8c2b7f8f2a084
khalloufiHamzaUFO <hamza.khalloufi@esprit.tn>
2023-11-05 12:12:49 +0100

    first commit

---
+++ src/main/java/com/example/apilogin/auth/AuthenticationService.java
@@ -0,0 +1,49 @@
+package com.example.apilogin.auth;
+
+import com.example.apilogin.service.JwtService;
+import com.example.apilogin.user.Role;
+import com.example.apilogin.user.User;
+import com.example.apilogin.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository ;
+    private final PasswordEncoder passwordEncoder ;
+    private final JwtService jwtService ;
+    private final AuthenticationManager manager;
+
+    public AuthenticationResponse register(RegisterRequest request){
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        manager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ src/main/java/com/example/apilogin/user/Role.java
@@ -0,0 +1,5 @@
+package com.example.apilogin.user;
+
+public enum Role {
+    ADMIN,USER
+}
